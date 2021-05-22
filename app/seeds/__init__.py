6d5c065c59c1da10c147b2132f6f14aa816e57a9
JoshuaJDevine <jd8386@gmail.com>
2021-05-21 18:43:37 -0700

    testing backend games routes

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-
+from .games import seed_games, undo_games
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -9,10 +9,12 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_games()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_games()
     # Add other undo functions here
