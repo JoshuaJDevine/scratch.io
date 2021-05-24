1059f0ef4b9612122ab8bb45b6f003d1006f4814
James Lentzsch <jrlentzsch@gmail.com>
2021-05-23 17:08:22 -0700

    teams seed and new team forms added

--- app/api/teams_routes.py
+++ app/api/teams_routes.py
@@ -1,27 +1,29 @@
 from flask import Blueprint, session
 from app.models import Team, db
 from app.forms import NewTeamForm
+from .auth_routes import validation_errors_to_error_messages
 
-team_routes = Blueprint('teams', __name__)
+teams_routes = Blueprint('teams', __name__)
 
 
 ############################# GET ALL TEAMS #############################
-@team_routes.route('/teams')
+@teams_routes.route('/teams')
 def teams():
     teams = Team.query.all()
     return {"teams": [team.to_dict() for team in teams]}
 
 ############################ GET ONE TEAM ###############################
-@team_routes.route('/team/<int:id>')
+@teams_routes.route('/team/<int:id>')
 def team(id):
     team = Team.query.get(id)
     return team.to_dict()
 
 
 ########################## POST NEW TEAM ################################
-@team_routes.route('/teams', methods='POST')
+@teams_routes.route('/teams', methods=['POST'])
 def new_team():
     form = NewTeamForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         team = Team(
             name=form.data['name'],
---
+++ app/forms/new_team_form.py
@@ -0,0 +1,15 @@
+from re import S
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms import validators
+from wtforms.validators import DataRequired
+from app.models import Team
+
+
+class NewTeamForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    blurb = TextAreaField('blurb')
+    avatar = StringField('avatar')
+    website = StringField('website')
+    github = StringField('github')
+    recruiting = BooleanField('recruiting')
