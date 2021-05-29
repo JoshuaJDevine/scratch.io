f221a72e13492b5e7fafad36d868fb83ed5461ff
James Lentzsch <jrlentzsch@gmail.com>
2021-05-29 14:02:26 -0700

    added team avatars folder and auto avatar seed for games

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -66,7 +66,7 @@ def sign_up():
         user = User(
             username=form.data['username'],
             email=form.data['email'],
-            # first_name=form.data['first_name'],
+            # first_namge=form.data['first_name'],
             # last_name=form.data['last_name'],
             # avatar=form.data['avatar'],
             # website=form.data['website'],
