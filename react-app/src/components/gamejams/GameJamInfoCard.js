38a9d7f34aa41a5c1391da215266e2597b06c23b
Christian Cozma <christiancozma.work@gmail.com>
2021-05-29 17:25:19 -0500

    rendered the children of the gamejam component

--- react-app/src/components/gamejams/GameJamInfoCard.js
+++ react-app/src/components/gamejams/GameJamInfoCard.js
@@ -7,25 +7,34 @@ export default function GameJamInfoCard({ game }) {
 // console.log(game);
     return (
         <>
-                <Box className="profile-gj-boxes">
-                        <Flex
-                          bg="rgba(4, 5, 10, 0.315)"
-                          columns={{ sm: 5 }}
-                          spacing="8"
-                          p="10"
-                          textAlign="center"
-                          rounded="lg"
-                          color="white">
-                              <Image
-                              src={game.avatar}
+                <Box className="profile-gj-boxes"
+                h="300px"
+                
+                >
+                            <p>{game.name}</p>
+                            <Image
+                              src={game?.avatar}
                               alt="alta-idk"
-                              h="100px"
+                              h="50px"
                               />
                             <p>{game.theme}</p>
-                    </Flex>
+                            <p>{game.blurb}</p>
+                            <p>{game?.website}</p>
+                            <p>{game?.github}</p>
+                            <p>{game.userLimit}</p>
+                            <p>{game.startDate}</p>
+                            <p>{game.endDate}</p>
                 </Box>
     </>
     )
 }
 
-
+{/* <Flex
+  bg="rgba(4, 5, 10, 0.315)"
+  h="300px"
+  spacing="8"
+  p="10"
+  textAlign="center"
+  rounded="lg"
+  color="white"> */}
+{/* </Flex> */}
