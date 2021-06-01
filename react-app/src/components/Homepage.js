2e59e1d04a055bb8df5a2de6c44f0170c084fcd5
Christian Cozma <christiancozma.work@gmail.com>
2021-05-31 22:17:46 -0500

    finished adding some styles to homepage

--- react-app/src/components/Homepage.js
+++ react-app/src/components/Homepage.js
@@ -1,13 +1,18 @@
 import React from "react"
-import GameJamBox from "./gamejams/GameJamBox";
-import GameBox from "./games/GameBox"
+// import GameJamBox from "./gamejams/GameJamBox";
+// import GameBox from "./games/GameBox"
+
+import { useDispatch} from "react-redux";
+import { useHistory } from "react-router-dom"
+import { login } from "../store/session"
 
 
 
 import {
     Flex,
     Box,
-    Heading
+    Heading,
+    Button
 } from "@chakra-ui/react"
 
 
@@ -18,6 +23,16 @@ export default function Homepage() {
         var image3 = "logo_I.png";
         var image4 = "logo_S.png";
         var image5 = "logo_K.png";
+
+        const dispatch = useDispatch();
+
+        const history = useHistory();
+
+        let demoUserPageLoader = () => {
+            dispatch(login('chad@aa.io', 'password'))
+            history.push(`/profile/${3}`)
+        }
+
     return (
 
         <Flex h="100vh" w="100vw" direction="column" classname="hp-main-body">
@@ -30,18 +45,27 @@ export default function Homepage() {
              <img src={path + image4}/>
              <img src={path + image5}/>
         </Box>
-        {/* <Box>
-            <Heading className="hp-heading">
-            <h1>
-            Sample Gamjams
-            </h1>
-            </Heading>
-            <GameJamBox />
-        </Box> */}
+
         <Box>
-            {/* <GameBox /> */}
+                <Box id="welcome-div">
+                <h1 id="welcome-msg">Welcome to Whisk!</h1>
+                <p id="welcome-msg-p"><div className="action-call">Fuel your creativity.</div>
+                <br /> — Join the Whisk community —
+                <br />A home to all game developers.
+                <br />Join us and share your story.</p>
         </Box>
+        </Box>
+                <Box  className="h-pg__sign-up-btn" ><Button variant="link" onClick={demoUserPageLoader}>Demo Login</Button></Box>
 
         </Flex>
     )
 }
+
+    // {/* <Box>
+    //         <Heading className="hp-heading">
+    //         <h1>
+    //         Sample Gamjams
+    //         </h1>
+    //         </Heading>
+    //         <GameJamBox />
+        // </Box> */}
